7ee2f07b664c80897b4d37f8ee2dc9e6ea69b786
dexat0r <nikit-balak@yandex.ru>
2022-09-20 22:05:38 +0300

    Trading bot

---
+++ src/index.ts
@@ -0,0 +1,155 @@
+import * as near from "near-api-js";
+import { FinalExecutionOutcome } from "near-api-js/lib/providers";
+import * as dotenv from "dotenv";
+import { Market, Tonic } from "@tonic-foundation/tonic";
+import { getNearConfig } from "@tonic-foundation/config";
+import chalk from "chalk";
+
+dotenv.config({
+    path: `./${
+        process.env.MODE === "development" ? "development" : "production"
+    }.env`,
+});
+
+const network = process.env.MODE === "development" ? "testnet" : "mainnet";
+
+const getGasUsage = (o: FinalExecutionOutcome) => {
+    const receiptGas = o.transaction_outcome.outcome.gas_burnt;
+    const actionGas = o.receipts_outcome.reduce(
+        (acc, x) => acc + x.outcome.gas_burnt,
+        0
+    );
+    return `${((receiptGas + actionGas) / Math.pow(10, 12)).toFixed(2)} TGas`;
+};
+
+async function monitorOrder(
+    tonic: Tonic,
+    marketId: string,
+    i = 5000
+): Promise<void> {
+    log(`Waiting until order succeed...`);
+    await new Promise<void>((resolve) => {
+        let timeStart = Date.now();
+        const interval = setInterval(async () => {
+            const orders = await tonic.getOpenOrders(marketId);
+            if (!orders.length) {
+                clearInterval(interval);
+                log(`Time passed: ${(Date.now() - timeStart) / 1000} seconds`);
+                resolve();
+            }
+        }, i);
+    });
+}
+
+async function getTokenBalance(
+    tonic: Tonic,
+    market: Market,
+    tokenId: string
+): Promise<number> {
+    const balances = await tonic.getBalances();
+    return market.quantityBnToNumber(balances[tokenId]);
+}
+
+function log(msg: any, ...args: unknown[]) {
+    console.log(
+        `${chalk.green(new Date().toLocaleTimeString())}: ${chalk.blue(
+            msg
+        )} ${args}`
+    );
+}
+
+async function main(): Promise<void> {
+    console.clear();
+    log(`Starting bot....`);
+    const { keyStores, KeyPair, connect } = near;
+
+    const ks = new keyStores.InMemoryKeyStore();
+    const kpair = KeyPair.fromString(process.env.PK!);
+    ks.setKey(network, process.env.ACCOUNT_ID!, kpair);
+
+    log(`Key was succesfuly added!`);
+
+    const config: near.ConnectConfig = {
+        ...getNearConfig(network),
+        keyStore: ks,
+    };
+    const provider = await connect(config);
+
+    log(`Connected to NEAR!`);
+
+    const account = await provider.account(process.env.ACCOUNT_ID!);
+    const tonic = new Tonic(account, process.env.CONTRACT_ID!);
+
+    const marketId = process.env.MARKET_ID!;
+    const market = await tonic.getMarket(marketId);
+    log(`Closing previous orders...`);
+    await tonic.cancelAllOrders(marketId);
+
+    log(`Starting work process!`);
+
+    while (true) {
+        const amountBuy = await getTokenBalance(
+            tonic,
+            market,
+            market.quoteTokenId
+        );
+        if (amountBuy > 0) {
+            log(
+                `Creating Buy order with amount ${amountBuy} ${
+                    market.quoteTokenId.split(".")[0]
+                }...`
+            );
+
+            try {
+                log("Sending transaction...");
+                const { executionOutcome: tx, response: _ } =
+                    await market.placeOrder({
+                        quantity: amountBuy,
+                        side: "Buy",
+                        orderType: "Limit",
+                        limitPrice: Number(process.env.LIMIT || 0.9999),
+                    });
+                log(`Gas usage: ${getGasUsage(tx)}`);
+            } catch (e) {
+                log("Order failed", e);
+                continue;
+            }
+
+            await monitorOrder(tonic, marketId);
+        }
+
+        const amountSell = await getTokenBalance(
+            tonic,
+            market,
+            market.baseTokenId
+        );
+
+        log(
+            `Creating Sell order with amount ${amountSell} ${
+                market.quoteTokenId.split(".")[0]
+            }...`
+        );
+        try {
+            log("Sending transaction...");
+            const { executionOutcome: tx, response: _ } =
+                await market.placeOrder({
+                    quantity: amountSell,
+                    side: "Sell",
+                    orderType: "Market",
+                });
+            log(`Gas usage: ${getGasUsage(tx)}`);
+        } catch (error) {
+            log("Order failed", error);
+        }
+
+        await monitorOrder(tonic, marketId);
+        log(
+            `Cycle ends! Received: ${
+                (await getTokenBalance(tonic, market, market.quoteTokenId)) -
+                amountBuy
+            }`
+        );
+    }
+}
+
+main();
